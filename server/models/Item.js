23523b117534cdd840fdb37fb5fc388d124b56e9
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2023-01-12 22:18:14 -0800

    add item in models

---
+++ server/models/Item.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const itemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  image: {
+    type: String
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0.99
+  },
+  quantity: {
+    type: Number,
+    min: 0,
+    default: 0
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  }
+})
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
