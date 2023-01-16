8cad1981e2644d7d80324ef15bc6cfc368899baa
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2023-01-15 21:07:36 -0800

    add resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,2 +1,56 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Item, Category, Order } = require('../models')
+const { signToken } = require('../utils/auth');
+const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+
+const resolvers = {
+  Query: {
+    categories: async () => {
+      return await Category.find();
+    },
+    items: async (parent, { category, name }) => {
+      const params = {};
+
+      if (category) {
+        params.category = category;
+      }
+
+      if (name) {
+        params.name = {
+          $regex: name
+        }
+      }
+
+      return await Item.find(params).populate('category');
+    },
+    item: async (parent, { _id }) => {
+      return await Item.findById(_id).populate('category');
+    },
+    user: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate({
+          path: 'orders.items',
+          populate: 'category'
+        });
+
+        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+
+        return user;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    order: async (parent, { _id }, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate({
+          path: 'orders.items',
+          populate: ' category'
+        });
+
+        return user.orders.id(_id);
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+  }
+}
